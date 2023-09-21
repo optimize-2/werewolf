679997102b12755f8abbe00aaf60b48a0ba0ca18
optimize-2 <55306215+optimize-2@users.noreply.github.com>
2023-09-21 21:54:14 +0800

    sb

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # werewolf
+
+## server -> client:
+
+`username` 和 `player` 都指用户名，游戏相关的我叫 `player`。
+
+
+### `recieveMessage`（做完了）
+
+参数：`{username: string, message: string}`
+
+### `updateUsers`（做完了）
+
+参数：`users: Record<string, PlayerState>`
+
+### `gameStart`（做完了）
+
+参数：`{ role: Role, users: Record<string, PlayerState>}`
+
+如果你在 `users` 里不是 `alive`（那么通常是 `spec`），那么说明你没参与这局游戏，`role` 无意义。
+
+### `loginResult`（做完了）
+
+参数：`state: PlayerState`
+
+## client -> server:
+
+### `login`
+
+参数：`username: string`
+
+### `sendMessage`
+
+参数：`message: string`
+
+### `disconnect`
+
+### `ready`
+
+```
+export type PlayerState = 
+        | 'unready'
+        | 'ready'
+        | 'alive'
+        | 'spec'
+
+export type Role =
+        | 'villager'
+        | 'werewolf'
+        | 'seer'
+        | 'witch'
+        | 'hunter'
+```
---
+++ backend/game.ts
@@ -0,0 +1,72 @@
+import { loadConfig, ConfigType } from "./config"
+import { log } from "./utils"
+
+export type PlayerState = 
+        | 'unready'
+        | 'ready'
+        | 'alive'
+        | 'spec'
+        | 'leave'
+
+export type Role =
+        | 'villager'
+        | 'werewolf'
+        | 'seer'
+        | 'witch'
+        | 'hunter'
+
+export type Target =
+        | 'villagers'
+        | 'gods'
+
+export type Period = 
+        | 'discuss'
+        | 'vote'
+        | 'kill'
+        | 'divine'
+
+const players: Record<string, PlayerState> = {}
+const roles: Record<string, Role> = {}
+
+export const getPlayers = () => players
+export const getRoles = () => roles
+
+let config: ConfigType
+let requiredPlayers = 0
+
+export let inGame = false
+
+export const loadGame = () => {
+    config = loadConfig()
+    Object.values(config.roles).forEach(e => {
+        requiredPlayers += e
+    })
+}
+
+export const setState = (player: string, state: PlayerState) => {
+    if (state !== 'leave') players[player] = state
+    else delete players[player]
+    return players
+}
+
+export const checkStart = () => {
+    if (inGame) return
+    const readyPlayers: Array<string> = []
+    Object.entries(players).forEach(([player, state]) => {
+        if (state === 'ready') {
+            readyPlayers.push(player)
+        }
+    })
+    if (readyPlayers.length === requiredPlayers) {
+        return inGame = true
+    }
+    return false
+}
+
+export const addPlayer = (player: string) => {
+    if (inGame) {
+        return players[player] = 'spec'
+    } else {
+        return players[player] = 'unready'
+    }
+}
