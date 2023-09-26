afe014e9af27f0b7930bf3b275cf2b64cb85b8cb
sunzhangchang <642311409@qq.com>
2023-09-26 19:36:56 +0800

    feat(frontend): :sparkles: vote result history

--- backend/index.ts
+++ backend/index.ts
@@ -79,7 +79,7 @@ const server = http.createServer((req, res) => {
 
 loadGame()
 
-const port = 1338
+const port = 1335
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
 })
@@ -310,5 +310,5 @@ export const sendWerewolfResult = () => {
 }
 
 export const sendSpecInfo = () => {
-    
+
 }
